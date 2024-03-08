4769bcf8adfd530579a18bd8ff7a714c49477ce1
Tamara Jordan <tamara.jordan@springernature.com>
2024-03-08 22:11:57 +0000

    Fix scripts

--- build.sh
+++ build.sh
@@ -2,10 +2,19 @@
 
 set -eo pipefail
 
-mod_dir=".release"
+repo_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+mod_dir="${repo_dir:?}/.release"
+linked_folder="${repo_dir:?}/.factorio-test-data/mods/crafting-combinator-tamj0rd2"
 
-rm -rf "${mod_dir}"
+rm -rf "${mod_dir:?}"/*
 npm run build
 cp mod_info.json "${mod_dir}/info.json"
+
+mkdir -p ".factorio-test-data/mods"
+
+#don't create a symlink if there's one already
+if [ ! -L "${linked_folder}" ] && [ ! -e "${linked_folder}" ]; then
+  ln -s "${mod_dir:?}" "${linked_folder:?}"
+fi
 
 echo "Built successfully"
--- test.sh
+++ test.sh
@@ -6,13 +6,7 @@ set -o pipefail
 ./build.sh
 mod_name="crafting-combinator-tamj0rd2"
 repo_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-mods_directory="${repo_dir}/.factorio-test-data/mods"
-mod_directory="${mods_directory}/${mod_name}"
 
-rm -rf "${mod_directory:?}"
-mkdir -p "${mods_directory}"
-cp -r "${repo_dir}/.release" "${mod_directory}"
-
-npx factorio-test run "${repo_dir}/.release" --data-directory "${repo_dir}/.factorio-test-data"
+npx factorio-test run --mod-name "${mod_name}" --data-directory "${repo_dir}/.factorio-test-data"
 
 echo "Tests completed successfully :)"
